5e7488f1fdf21be7354be687deb444dad6707ce9
LateDevonian <agean27@gmail.com>
2020-08-21 12:00:26 +1000

    Fleshed out repo a bit more

---
+++ groove/groove.go
@@ -0,0 +1,7 @@
+package groove
+
+func (r Danceinstructor) groove() {
+	//this function will take the current direction and grove one
+	//dancespace in that direction and will return the new location
+
+}
